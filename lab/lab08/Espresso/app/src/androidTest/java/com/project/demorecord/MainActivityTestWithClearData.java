718a05bcb7f35be576aafbf672572fe792c46a9b
belizwp <belizwp@live.com>
2017-10-26 14:04:00 +0700

    independently testcase

--- lab/lab08/Espresso/app/src/androidTest/java/com/project/demorecord/MainActivityTestWithClearData.java
+++ lab/lab08/Espresso/app/src/androidTest/java/com/project/demorecord/MainActivityTestWithClearData.java
@@ -1,5 +1,8 @@
 package com.project.demorecord;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.support.v7.widget.RecyclerView;
@@ -8,6 +11,7 @@ import android.view.View;
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -25,10 +29,20 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
 
 @RunWith(AndroidJUnit4.class)
-public class MainActivityTest {
+public class MainActivityTestWithClearData {
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    /**
+     * clear data
+     */
+    @Before
+    public void clear() {
+        Context context = InstrumentationRegistry.getTargetContext();
+        SharedPreferences sp = context.getSharedPreferences(CommonSharePreference.NAME, Context.MODE_PRIVATE);
+        sp.edit().clear().commit();
+    }
 
     /**
      * โดยไม่กรอก Name และ Age กดปุ่ม ADDED
